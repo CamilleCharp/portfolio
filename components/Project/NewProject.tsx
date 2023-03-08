30d3e6c19bb9783361791fdef84295ffe8b78ed7
Camille Charpentier <camille.charpentier77@gmail.com>
2023-03-08 16:28:21 +0100

    new project page, fixed about but

---
+++ components/Project/NewProject.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import styles from "./NewProject.module.scss";
+import { Roboto_Condensed } from "next/font/google";
+import Image from "next/image";
+
+const roboto_condensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["300", "700"],
+});
+export function NewProject({
+  emphasedTitle,
+  title,
+  description,
+  projectUrl = "#",
+  background,
+}: any) {
+  return (
+    <div className={styles.project}>
+      <a href={projectUrl}>
+        <div className={styles.project__background}>
+          <Image
+            className={styles.background__image}
+            src={background}
+            alt=""
+            width={360}
+            height={16 * 20}
+          />
+        </div>
+        <div className={styles.project__content}>
+          <h1
+            className={`${roboto_condensed.className} ${styles.project__title}`}
+          >
+            <span className="font-bold">{emphasedTitle}</span> {title}
+          </h1>
+
+          <p className={styles.project__description}>{description}</p>
+        </div>
+      </a>
+    </div>
+  );
+}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,28 @@
-import { Project } from "./Project/Project";
 import styles from "./Projects.module.scss";
+import { NewProject } from "./Project/NewProject";
 
 export function Projects() {
   return (
     <section className={styles.projects} id="projects">
-      <Project
-        emphasedTitle="Paper"
-        title="Recipes"
-        description="I built a recipe sharing website using Next.js, Prisma, and Supabase. The website is designed to allow users to easily share and discover new recipes, while also providing an opportunity for me to deepen my understanding of React and Next.js."
-        background="/paper-recipes.jpg"
-        projectUrl="https://paper-recipes.ovh"
-      />
-      <Project
-        emphasedTitle="Browsershot"
-        description="A chrome extension I made to make it easier to take elegant screenshot of web pages"
-        background="/browsershot.jpg"
-        projectUrl="https://chrome.google.com/webstore/detail/browsershot/bcbbjngkibonjfkndbmicdobedcdgmhk?hl=fr"
-      />
+      <div className={styles.project__presentation}>
+        <h1 className="section-title">My projects</h1>
+        <h2 className="section-emphasis">The latests projects I worked on</h2>
+      </div>
+      <div className={styles.projects__content}>
+        <NewProject
+          emphasedTitle="Paper"
+          title="Recipes"
+          description="A website I built to easily share recipes and deepen my knowledge of React/NextJS. It use NextAuth to handle authentication, and Supabase coupled with Prisma to manage datas"
+          background="/paper-recipes.jpg"
+          projectUrl="https://paper-recipes.ovh"
+        />
+        <NewProject
+          emphasedTitle="Browsershot"
+          description="A chrome extension I made to make it easier to take elegant screenshot of web pages"
+          background="/browsershot.jpg"
+          projectUrl="https://chrome.google.com/webstore/detail/browsershot/bcbbjngkibonjfkndbmicdobedcdgmhk?hl=fr"
+        />
+      </div>
     </section>
   );
 }
